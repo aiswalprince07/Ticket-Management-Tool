0ea05fb9c8f63397d0921ff00e0c49d7f1bb91a6
aiswalprince07 <81444348+aiswalprince07@users.noreply.github.com>
2023-06-03 16:12:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let addbtn = document.querySelector(".add-btn");
+addbtn.addEventListener("click",(e)=>{
+    //Display Model
+    
+    //Generate ticket
+
+})
+
+let removebtn = document.querySelector(".remove-btn");
+removebtn.addEventListener("click",(e)=>{
+    // console.log("clicked");
+
+})
+
